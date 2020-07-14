067f11dc29a958b7d59ff5b268c179528f7da715
2dx-eko <47917932+2dx-eko@users.noreply.github.com>
2020-07-14 22:06:11 +0900

    Add files via upload

--- app/controller/TodoController.php
+++ app/controller/TodoController.php
@@ -9,14 +9,16 @@ class TodoController{
         $title = $_GET["title"];
         $status = $_GET["status"];
         $status = (int) $status;
-
-        $select = [
-            "title" => $title,
-            "status" => $status,
-        ];
         
+        $params = [];
+        if($title) {
+            $params = array_merge($params,array("title" => $title));
+        }
+        if($status) {
+            $params = array_merge($params,array("status" => $status));
+        }
         //$query = $this->getQuery($title,$status);
-        $query = $this->getQuery($select);
+        $query = $this->getQuery($params);
 
         //var_dump($query);
         $todo_list = Todo::findByQuery($query);//一部取得
@@ -26,22 +28,25 @@ class TodoController{
     }
 
     //↑$title $statusを受け取ってDBから検索するメソッド
-    public function getQuery($select){
-        foreach($select as $key => $value){
-            if(!empty($value)){//タイトルのみが入ってるとき
-                echo "<pre>";
-                var_dump($key);
-                echo "</pre>";
-                $query = "SELECT * FROM todos WHERE title = '" . $value . "'";
+    public function getQuery($params){
+        foreach($params as $key => $value){
+            if(!empty($value)){//タイトルかstatusが入ってるとき
+                $query = "SELECT * FROM todos WHERE $key = '" . $params["$key"] . "'";
+            }
 
-            }/*else if(empty($value) && $select["status"] == 1 || 2){//ステータスのみが入ってるとき
-
-                $query = "SELECT * FROM todos WHERE status =" . $select["status"];
-            }*/
-            
-    
+            if(!empty($params["title"]) &&!empty($params["status"])){
+                $query = "SELECT * FROM todos WHERE title = '" . $title . "'" . "AND status =" . $status;
+            }
          
         }
+        return $query;
+
+        /*else if($params["{$key}"] == 1 || 2){//ステータスのみが入ってるとき
+            echo "<pre>";
+            var_dump($params);
+            echo "</pre>";
+            $query = "SELECT * FROM todos WHERE status =" . $params["$key"];
+        }*/
 
 
         /*if(isset($title) && $status == 0){//タイトルのみが入ってるとき
@@ -56,7 +61,7 @@ class TodoController{
 
             $query = "SELECT * FROM todos WHERE title = '" . $title . "'" . "AND status =" . $status;
         }*/
-        return $query;
+        
     }
 }
 ?>
--- app/model/todo.php
+++ app/model/todo.php
@@ -29,7 +29,4 @@ class Todo{
         return $todo_list;
     }
 }
-
-
-
 ?>
