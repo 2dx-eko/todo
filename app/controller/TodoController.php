000b46f32c7606094027c4452c810a174b5ddc72
2dx-eko <47917932+2dx-eko@users.noreply.github.com>
2020-07-18 10:41:23 +0900

    Add files via upload

--- app/controller/TodoController.php
+++ app/controller/TodoController.php
@@ -20,7 +20,6 @@ class TodoController{
         //$query = $this->getQuery($title,$status);
         $query = $this->getQuery($params);
 
-        //var_dump($query);
         $todo_list = Todo::findByQuery($query);//一部取得
 
         return $todo_list;
@@ -29,39 +28,23 @@ class TodoController{
 
     //↑$title $statusを受け取ってDBから検索するメソッド
     public function getQuery($params){
+        $where = "WHERE ";
+        $and = " AND ";
         foreach($params as $key => $value){
-            if(!empty($value)){//タイトルかstatusが入ってるとき
-                $query = "SELECT * FROM todos WHERE $key = '" . $params["$key"] . "'";
+            if(!empty($value)){
+                $where = $where . sprintf("%s = '%s'", $key, $value);
             }
-
-            if(!empty($params["title"]) &&!empty($params["status"])){
-                $query = "SELECT * FROM todos WHERE title = '" . $title . "'" . "AND status =" . $status;
+            
+            //↓ループが最後以外の時にandがたされる！！！！！
+            if($value !== end($params)){
+                $where = $where . $and;
             }
-         
         }
+        $query = sprintf("SELECT * FROM todos %s", $where);
+        var_dump($query);
+
         return $query;
-
-        /*else if($params["{$key}"] == 1 || 2){//ステータスのみが入ってるとき
-            echo "<pre>";
-            var_dump($params);
-            echo "</pre>";
-            $query = "SELECT * FROM todos WHERE status =" . $params["$key"];
-        }*/
-
-
-        /*if(isset($title) && $status == 0){//タイトルのみが入ってるとき
-
-            $query = "SELECT * FROM todos WHERE title = '" . $title . "'";
-
-        }else if(empty($title) && $status == 1 || 2){//ステータスのみが入ってるとき
-
-            $query = "SELECT * FROM todos WHERE status =" . $status;
-
-        }else if(isset($title) && $status == 1 || 2){//両方が入ってる時   
-
-            $query = "SELECT * FROM todos WHERE title = '" . $title . "'" . "AND status =" . $status;
-        }*/
-        
+        //$query = "SELECT * FROM todos WHERE title = '" . $title . "'" . "AND status =" . $status;       
     }
 }
 ?>
